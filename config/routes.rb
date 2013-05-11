beed2cca1b00e099a01ac4af00827e0f918098ce
Alexandr Piskunov <piskunov1984@mail.ru>
2013-05-11 22:03:59 +0400

    Added Translation For Index Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 HomeAccountsDepartment::Application.routes.draw do
-  resources :expenses
-
+  
+  scope "(:locale)", locale: /en|ru/ do	
+    resources :expenses
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,6 +54,8 @@ HomeAccountsDepartment::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'expenses#index'
+  
+  match ':locale' => 'expenses#index'
 
   # See how all your routes lay out with "rake routes"
 
